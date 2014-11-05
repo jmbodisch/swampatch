f209b5fbe7015f80d0ee579654da3862ce5f5cfd
atreusna <atreusna@gmail.com>
2014-11-04 21:24:13 -0500

    Update game.js

--- Experiments/game.js
+++ Experiments/game.js
@@ -3,6 +3,7 @@ var height = 12;
 var numOfColors = 5;        // easy mode = 5; hard = 6
 var well = [];
 var previousRow = [];       // This is needed to ensure that adjacent generated blocks are not the same color
+var cursorPos = 0;          // 2-wide cursor; the number corresponds to the block the left half of the cursor is on
 
 function initializeWell() {     //Well is empty and of size 0; "inflate" it with 0's (blank) to the right size
     for (var i = 0; i < (width * height); i++) {
@@ -35,15 +36,59 @@ function generateRow() {        //This will create a row of random blocks
     
 }
 
-function displayWell() {        
-    for (var i = 0; i < height; i++) {
+function displayWell() {
+    var currentPos;        
+    for (var i = height - 1; i >= 0; i--) {
         for (var j = 0; j < width; j++) {
-            document.write(well[(i*width)+j] + " ");
+            currentPos = (i*width+j);
+            if (currentPos == cursorPos || currentPos == cursorPos+1)
+                document.write("<span style=\"color:white ; background-color:black\">" + well[currentPos] + "</span>" + " ");
+            else
+                document.write(well[currentPos] + " ");
         }
         document.write("<br>");
     }
 }
 
-initializeWell();
-generateWell();
-displayWell();
+function moveCursorDown() {
+    if (cursorPos - width < 0)                  // Cursor is at the bottom row
+        return;
+    else
+        cursorPos = cursorPos - width;
+}
+
+function moveCursorUp() {
+    if (cursorPos + width > width * height - 1) // Cursor is at the top row
+        return;
+    else
+        cursorPos = cursorPos + width;
+}
+
+function moveCursorLeft() {
+    if (cursorPos % width == 0)                 // Cursor is on the left wall
+        return;
+    else
+        cursorPos = cursorPos - 1;
+}
+
+function moveCursorRight() {
+    if ((cursorPos + 2) % width == 0)           // Cursor is on the right wall
+        return;
+    else
+        cursorPos = cursorPos + 1;
+}
+
+function swapBlocks() {
+    var tempValue;
+    tempValue = well[cursorPos];
+    well[cursorPos] = well[cursorPos + 1];
+    well[cursorPos + 1] = tempValue;
+}
+
+function initializePage() {
+    initializeWell();
+    generateWell();
+    displayWell();
+}
+
+initializePage();
